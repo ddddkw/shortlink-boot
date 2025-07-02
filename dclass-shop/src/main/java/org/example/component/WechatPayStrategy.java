e486c3885d7adbce7ca36ad62321b499db441ed9
doukw <2200600836@qq.com>
2025-07-02 17:44:07 +0800

    取消订单等方法实现

--- dclass-shop/src/main/java/org/example/component/WechatPayStrategy.java
+++ dclass-shop/src/main/java/org/example/component/WechatPayStrategy.java
@@ -4,12 +4,14 @@ import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.example.config.WeChatPayApi;
 import org.example.config.WechatPayConfig;
+import org.example.utils.CommonUtil;
 import org.example.vo.PayInfoVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -85,16 +87,90 @@ public class WechatPayStrategy implements PayStrategy{
 
     @Override
     public String refund(PayInfoVO payInfoVO) {
-        return PayStrategy.super.refund(payInfoVO);
+        String outTradeNo = CommonUtil.getStringNumRandom(32);
+        String url = String.format(WeChatPayApi.NATIVE_QUERY,outTradeNo,wechatPayConfig.getWxPayAppid());
+
+        HttpGet httpget = new HttpGet(url);
+        httpget.setHeader("Accept","application/json");
+        String result = "";
+        try(CloseableHttpResponse httpResponse = wechatPayClient.execute(httpget);) {
+            // 响应码
+            int responseCode = httpResponse.getStatusLine().getStatusCode();
+            // 响应体
+            String responseStr = EntityUtils.toString(httpResponse.getEntity());
+            log.info("查询响应码：{}，响应体：{}",responseCode,responseStr);
+            if (responseCode == HttpStatus.SC_OK) {
+                JSONObject jsonObject = JSONObject.parseObject(responseStr);
+                if (jsonObject.containsKey("trade_state"))
+                    result = jsonObject.getString("trade_state");
+            } else {
+                log.error("查询响应失败：{}，响应体：{}",responseCode,responseStr);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
+    /**
+     * 查询支付状态
+     * @param payInfoVO
+     * @return
+     */
     @Override
     public String queryPayStatus(PayInfoVO payInfoVO) {
-        return PayStrategy.super.queryPayStatus(payInfoVO);
+        String url = String.format(WeChatPayApi.NATIVE_QUERY,payInfoVO.getOutTradeNo(),wechatPayConfig.getWxPayAppid());
+
+        HttpGet httpget = new HttpGet(url);
+        httpget.setHeader("Accept","application/json");
+        String result = "";
+        try(CloseableHttpResponse httpResponse = wechatPayClient.execute(httpget);) {
+            // 响应码
+            int responseCode = httpResponse.getStatusLine().getStatusCode();
+            // 响应体
+            String responseStr = EntityUtils.toString(httpResponse.getEntity());
+            log.info("查询响应码：{}，响应体：{}",responseCode,responseStr);
+            if (responseCode == HttpStatus.SC_OK) {
+                JSONObject jsonObject = JSONObject.parseObject(responseStr);
+                if (jsonObject.containsKey("trade_state"))
+                    result = jsonObject.getString("trade_state");
+            } else {
+                log.error("查询响应失败：{}，响应体：{}",responseCode,responseStr);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Override
     public String closeOrder(PayInfoVO payInfoVO) {
-        return PayStrategy.super.closeOrder(payInfoVO);
+        Map payObject = new HashMap();
+        payObject.put("mchid",wechatPayConfig.getMchId());
+
+        String body = payObject.toString();
+
+        log.info("请求参数：{}",body);
+
+        StringEntity entity = new StringEntity(body,"utf-8");
+        entity.setContentType("application/json");
+
+        String url = String.format(WeChatPayApi.NATIVE_CLOSE,payInfoVO.getOutTradeNo());
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setHeader("Accept","application/json");
+        httpPost.setEntity(entity);
+        String result = "";
+        try(CloseableHttpResponse httpResponse = wechatPayClient.execute(httpPost)) {
+            int responseCode = httpResponse.getStatusLine().getStatusCode();
+            log.info("关闭订单响应码：{}",responseCode);
+            if (responseCode == HttpStatus.SC_NO_CONTENT) {
+                result = "CLOSE_SUCCESS";
+            } else {
+                log.error("关闭订单响应失败：{}",responseCode);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
